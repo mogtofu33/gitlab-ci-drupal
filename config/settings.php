98cc0c9b223e46a97a9f6aa276fc4d91bb78b5e8
Jean Valverde <jeanvalverde@catalyst.net.nz>
2018-07-06 14:47:47 +1200

    [skip ci] Try to fix Drupal install

---
+++ config/settings.php
@@ -0,0 +1,24 @@
+<?php
+
+$settings['hash_salt'] = '3UL8Ex5rP_ZgBh-ZaQ_PgQ6CTzdWXBWWviBsrobVTLxEAA6H2kvtBkYaUNkZq--LmvV-h0M_6w';
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+$settings['entity_update_batch_size'] = 50;
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = '../config/sync';
+
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
