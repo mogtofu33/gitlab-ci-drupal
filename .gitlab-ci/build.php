28ab3b7b0eed755fcbda56293c218da2c60e4365
Jean Valverde <moimog33@gmail.com>
2020-06-03 06:47:57 +0000

    Add composer examples.

--- .gitlab-ci/build.php
+++ .gitlab-ci/build.php
@@ -9,13 +9,28 @@
  * the regular composer install if a project.
  *
  * Examples:
+ *
  * $this->say("This will be run in Build script!");
+ *
  * $this->taskPack('build.zip')
  *   ->add('vendor')
  *   ->add('web')
  *   ->run();
+ *
  * $this->taskGulpRun()
  *   ->dir($this->webRoot . 'themes/my_theme_with_gulp_task')
  *   ->run();
+ * 
+ * $this->taskComposerRequire()
+ *   ->noInteraction()
+ *   ->noAnsi()
+ *   ->workingDir($this->docRoot);
+ *   ->dependency('drupal/webform', '^5.13')
+ *   ->run();
+ *
+ * Or shortcut method in the RoboFile.php with this project:
+ * $this->composerRequire()
+ *  ->dependency('drupal/webform', '^5.13')
+ *  ->run();
  *
  */
